9e09e8bfeb9992e076c4d71d2883b40fadf45d55
Your Name <sakistr69@gmail.com>
2025-05-31 15:55:10 +0300

    hosted in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://thanosdev.com/Weather-Typescript",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
